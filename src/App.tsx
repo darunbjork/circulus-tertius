43870ceb4031d25f93eae626b49d1ec90d2b14e0
Mosho <moises.leon@chasacademy.se>
2025-10-30 13:45:28 +0100

    ahmads kod

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { io } from "socket.io-client";
+
+// Vi vill vid start av appen, automatiskt ansluta till servern
+const socket = io("ws://10.100.2.139:3001");
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [connected, setConnected] = useState(false);
+  const [messages, setMessages] = useState<string[]>([]);
+
+  const connectionStatus = connected ? "Connected" : "Disconnected";
+
+  useEffect(() => {
+    // När en anslutning har upprättats
+    socket.on("connect", () => {
+      setConnected(true);
+    });
+
+    // När anslutningen har avslutats
+    socket.on("disconnect", () => {
+      setConnected(false);
+    });
+
+    socket.on("cir-ter", (data: string) => {
+      console.log("Data received: ", data);
+
+      // const receivedMessage = JSON.parse(data); // Omvandlar JSON-string till objekt/array
+
+      setMessages((prev) => [...prev, JSON.stringify(data)]);
+
+      console.log(messages);
+    });
+
+    return () => {
+      // Kod som körs när komponenten avmonteras
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("cir-ter");
+    };
+  }, []);
+
+  const emitEvent = () => {
+    const message = {
+      message: "Hej där",
+      sender: "Ahmad ardal",
+    };
+
+    const stringifiedMessage = JSON.stringify(message);
+
+    socket.emit("cir-ter", stringifiedMessage);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div id="messages-container">
+      <p>{connectionStatus}</p>
+
+      {messages.map((message) => (
+        <p>{message}</p>
+      ))}
+    </div>
+  );
 }
 
-export default App
+export default App;
